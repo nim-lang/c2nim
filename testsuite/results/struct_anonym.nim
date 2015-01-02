8d5bdf4e746488fbe4f049329d718814c7c0ca34
Araq <rumpf_a@web.de>
2015-01-02 10:27:57 +0100

    'nimrod.cfg' is now 'c2nim.nim.cfg'

--- testsuite/results/struct_anonym.nim
+++ testsuite/results/struct_anonym.nim
@@ -3,23 +3,23 @@ type
     a*: cint
     b*: cint
 
-  INNER_C_UNION_4161638671* = object  {.union.}
+  INNER_C_UNION_2814606598* = object  {.union.}
     b*: cint
 
-  INNER_C_STRUCT_3680257704* = object 
-    a_union_in_the_struct*: INNER_C_UNION_4161638671
+  INNER_C_STRUCT_3682850820* = object 
+    a_union_in_the_struct*: INNER_C_UNION_2814606598
     c*: cint
 
-  INNER_C_STRUCT_3403983474* = object 
+  INNER_C_STRUCT_2821978930* = object 
     e*: cint
 
-  INNER_C_UNION_1138184789* = object  {.union.}
+  INNER_C_UNION_3201809032* = object  {.union.}
     d*: cint
-    a_struct_in_the_union*: INNER_C_STRUCT_3403983474
+    a_struct_in_the_union*: INNER_C_STRUCT_2821978930
 
   outerStruct* = object 
     a_nomal_one*: normal
     a*: cint
-    ano_388512601*: INNER_C_STRUCT_3680257704
-    a_union*: INNER_C_UNION_1138184789
+    ano_388512601*: INNER_C_STRUCT_3682850820
+    a_union*: INNER_C_UNION_3201809032
 
