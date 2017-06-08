7b0b4d87c94a73874ff5815210eb129daadc5064
ratiotile <eujain@gmail.com>
2017-06-08 11:29:17 -0400

    made parser accept C++11 defaulted constructor, resolves #88

--- cparse.nim
+++ cparse.nim
@@ -2172,6 +2172,11 @@ proc parseConstructor(p: var Parser, pragmas: PNode, isDestructor: bool;
     let body = compoundStatement(p)
     if pfKeepBodies in p.options.flags:
       result.sons[bodyPos] = body
+  of pxAsgn:
+    # '= default;' C++11 defaulted constructor
+    getTok(p)
+    eat(p, pxSymbol)
+    eat(p, pxSemicolon)
   else:
     parMessage(p, errTokenExpected, ";")
   if result.sons[bodyPos].kind == nkEmpty:
