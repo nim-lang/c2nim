22c862aa1f7dcdb0fffd4eac9bfafaf18325dd5e
Nouredine Hussain <hussain@icare.ch>
2017-10-17 19:24:02 +0200

    Fix #112

--- testsuite/results/discardables.nim
+++ testsuite/results/discardables.nim
@@ -2,8 +2,8 @@ proc AddPoint*(s: ptr Sizer; x: cint; y: cint): bool {.discardable.}
 proc SetSize*(w: ptr Widget; w: cint; h: cint): cint {.discardable.}
 ##  bug # #18
 
-var diamond*: array[4, array[2, GLfloat]] = [[0.0, 1.0], [1.0, 0.0], [0.0, - 1.0],
-                                       [- 1.0, 0.0]]
+var diamond*: array[4, array[2, GLfloat]] = [[0.0, 1.0], [1.0, 0.0], [0.0, -1.0],
+                                       [-1.0, 0.0]]
 
 ##  Left point
 ##  bug #40
