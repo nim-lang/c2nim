9c1ec403520488c6678fdb226f9e85910b9c29ff
Araq <rumpf_a@web.de>
2018-07-07 02:50:03 +0200

    make tests green again

--- testsuite/results/struct_anonym.nim
+++ testsuite/results/struct_anonym.nim
@@ -3,23 +3,23 @@ type
     a*: cint
     b*: cint
 
-  INNER_C_UNION_3731612527* {.bycopy.} = object {.union.}
+  INNER_C_UNION_191243455* {.bycopy.} = object {.union.}
     b*: cint
 
-  INNER_C_STRUCT_3723351023* {.bycopy.} = object
-    a_union_in_the_struct*: INNER_C_UNION_3731612527
+  INNER_C_STRUCT_182981951* {.bycopy.} = object
+    a_union_in_the_struct*: INNER_C_UNION_191243455
     c*: cint
 
-  INNER_C_STRUCT_217232430* {.bycopy.} = object
+  INNER_C_STRUCT_971830654* {.bycopy.} = object
     e*: cint
 
-  INNER_C_UNION_192447914* {.bycopy.} = object {.union.}
+  INNER_C_UNION_947046138* {.bycopy.} = object {.union.}
     d*: cint
-    a_struct_in_the_union*: INNER_C_STRUCT_217232430
+    a_struct_in_the_union*: INNER_C_STRUCT_971830654
 
   outerStruct* {.bycopy.} = object
     a_nomal_one*: normal
     a*: cint
-    ano_3764658544*: INNER_C_STRUCT_3723351023
-    a_union*: INNER_C_UNION_192447914
+    ano_224289472*: INNER_C_STRUCT_182981951
+    a_union*: INNER_C_UNION_947046138
 
