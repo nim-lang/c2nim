eb2a202bc768e92f5b1aefd8627f7dd438466238
Neelesh Chandola <neelesh.chandola@outlook.com>
2018-10-28 20:57:36 +0530

    Fix tests

--- testsuite/results/constructorcall.nim
+++ testsuite/results/constructorcall.nim
@@ -5,5 +5,7 @@ type
 
 
 proc constructfoo*(i: cint): foo {.constructor.}
-proc bar*(f: foo = constructfoo(0)): cint
+proc bar*(f: foo = constructfoo(0)): cint =
+  discard
+
 proc bar*(f: fooBar = constructfooBar(0)): cint
