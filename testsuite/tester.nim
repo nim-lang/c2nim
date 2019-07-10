92c8f67c0b950c44223a79a791551df1ad1b9640
ratiotile <eujain@gmail.com>
2019-07-10 18:14:07 -0400

    added to tester the ability to run selected tests.

--- testsuite/tester.nim
+++ testsuite/tester.nim
@@ -1,19 +1,45 @@
 # Small program that runs the test cases
 
-import strutils, os
+import strutils, os, parseopt
 
 const
   c2nimCmd = "c2nim $#"
   cpp2nimCmd = "c2nim --cpp $#"
   hpp2nimCmd = "c2nim --cpp --header $#"
   dir = "testsuite/"
+  usage = """
+c2nim test runner
+Usage: tester testnames [options]
+  Runs all tests by default without any arguments given.
+  If testnames are given, all othere tests will be skipped. Testnames are the 
+  test file names without extension.
+Options:
+  -h --help        Shows this help
+"""
 
 var
   failures = 0
+  exit_early = false
+  infiles = newSeq[string](0)
+
+for kind, key, val in getopt():
+  case kind
+  of cmdArgument:
+    infiles.add key
+  of cmdLongOption, cmdShortOption:
+    case key.normalize
+    of "help", "h":
+      stdout.write(usage)
+      exit_early = true
+  else:
+    stdout.writeLine("[Error] unknown option: " & key)
 
 proc test(t, cmd: string) =
+  let (_, name, _) = splitFile(t)
+  if infiles.len() > 0 and not (name in infiles):
+    return
   if execShellCmd(cmd % t) != 0: quit("FAILURE")
-  let nimFile = splitFile(t).name & ".nim"
+  let nimFile = name & ".nim"
   if readFile(dir & "tests" / nimFile) != readFile(dir & "results" / nimFile):
     echo "FAILURE: files differ: ", nimFile
     discard execShellCmd("diff -uNdr " & dir & "results" / nimFile & " " & dir & "tests" / nimFile)
@@ -23,9 +49,14 @@ proc test(t, cmd: string) =
   else:
     echo "SUCCESS: files identical: ", nimFile
 
-for t in walkFiles(dir & "tests/*.c"): test(t, c2nimCmd)
-for t in walkFiles(dir & "tests/*.h"): test(t, c2nimCmd)
-for t in walkFiles(dir & "tests/*.cpp"): test(t, cpp2nimCmd)
-for t in walkFiles(dir & "tests/*.hpp"): test(t, hpp2nimCmd)
+if not exit_early:
+  for t in walkFiles(dir & "tests/*.c"): 
+    test(t, c2nimCmd)
+  for t in walkFiles(dir & "tests/*.h"): 
+    test(t, c2nimCmd)
+  for t in walkFiles(dir & "tests/*.cpp"): 
+    test(t, cpp2nimCmd)
+  for t in walkFiles(dir & "tests/*.hpp"): 
+    test(t, hpp2nimCmd)
 
-if failures > 0: quit($failures & " failures occurred.")
+  if failures > 0: quit($failures & " failures occurred.")
