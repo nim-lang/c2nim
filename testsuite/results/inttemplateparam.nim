d264a5470d573e23266b1e2a7311e7caf1fe6c98
Christopher Dunn <cdunn2001@gmail.com>
2020-09-24 16:02:00 -0500

    Test fix for #183

--- testsuite/results/inttemplateparam.nim
+++ testsuite/results/inttemplateparam.nim
@@ -1,2 +1,2 @@
-proc foo*[T; N: static[cint]; U: static[csize]; C; CA: static[clong]; S: static[cshort];
+proc foo*[T; N: static[cint]; U: static[csize_t]; C; CA: static[clong]; S: static[cshort];
          T2; LL: static[clonglong]; UI: static[cuint]; US: static[cushort]]()
