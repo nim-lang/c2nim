c5263398840f0ae5825be9c3a4e62681f082197b
Araq <rumpf_a@web.de>
2021-05-30 07:00:23 +0200

    closes #41

--- testsuite/results/vincenth.nim
+++ testsuite/results/vincenth.nim
@@ -8,3 +8,11 @@ type
 ##  C11 init syntax:
 
 let lookup*: array[2, foo] = [0: (x: 1, y: 3, z: 4), 1: (x: 2, y: 3, z: 4)]
+
+type
+  message_type* = enum
+    MESSAGE_TYPE_NOTICE, MESSAGE_TYPE_PRIVMSG, MESSAGE_TYPE_COUNT
+
+
+let cmdname*: array[MESSAGE_TYPE_COUNT, cstring] = [MESSAGE_TYPE_PRIVMSG: "PRIVMSG",
+    MESSAGE_TYPE_NOTICE: "NOTICE"]
--- testsuite/tests/vincenth.h
+++ testsuite/tests/vincenth.h
@@ -7,3 +7,14 @@ const foo lookup[2] = {
     [0] = {.x = 1, .y = 3, .z = 4},
     [1] = {.x = 2, .y = 3, .z = 4}
 };
+
+enum message_type {
+       MESSAGE_TYPE_NOTICE,
+       MESSAGE_TYPE_PRIVMSG,
+       MESSAGE_TYPE_COUNT
+};
+
+const char *cmdname[MESSAGE_TYPE_COUNT] = {
+    [MESSAGE_TYPE_PRIVMSG] = "PRIVMSG",
+    [MESSAGE_TYPE_NOTICE] = "NOTICE",
+};
