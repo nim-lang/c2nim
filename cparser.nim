21f26bfc19404ec1f40e9eb29448092eff880fb7
Jaremy Creechley <creechley@gmail.com>
2023-02-19 00:53:38 -0700

    fix c importc to discard function bodies

--- cparser.nim
+++ cparser.nim
@@ -2082,7 +2082,6 @@ proc declarationWithoutSemicolon(p: var Parser; genericParams: PNode = emptyNode
           addSon(result, newNodeP(nkDiscardStmt, p))
         addSon(result.lastSon, emptyNode)
       else:
-        # if pfCDecl in p.options.flags or pfImportc in p.options.flags:
         if pfImportc in p.options.flags:
           discard compoundStatement(p)
           addSon(result, emptyNode)
