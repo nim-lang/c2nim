5fa609f1b9e6948dcd8b0decea45b72ed16e68c1
Jaremy Creechley <creechley@gmail.com>
2023-04-11 21:33:11 -0700

    initial create

---
+++ testsuite/cextras/rcl_allocator_imp_pragma.h
@@ -0,0 +1,23 @@
+
+//
+//The default allocator uses malloc(), free(), calloc(), and realloc().
+typedef struct rcutils_allocator_s
+{
+
+  /// allocate: Allocate memory, given a size and the `state` pointer.
+  void * (*allocate)(size_t size, void * state);
+
+  /** allocator objects.  */
+  void * state;
+
+
+} rcutils_allocator_t;
+
+
+/// Return a zero initialized allocator.
+/**
+ * Note that this is an invalid allocator and should only be used as a placeholder.
+ */
+rcutils_allocator_t rcutils_get_zero_initialized_allocator(void);
+
+
